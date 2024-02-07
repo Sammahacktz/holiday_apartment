fca31cdac77790b89c244f78c12239c82e1e1423
chre029b <charlie-luis.reichelt@tu-dresden.de>
2024-02-07 18:29:15 +0100

    great working thing

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-var stars = document.getElementById("background");
-var moon = document.getElementById("background2");
-var mountains_behind = document.getElementById("house");
-var mountains_front = document.getElementById("front");
 
+var parallax = [
+    document.getElementById("sky"),
+    document.getElementById("clouds"),
+    document.getElementById("hill_house"),
+    document.getElementById("hill_small"),
+    document.getElementById("hill_trees"),
+    document.getElementById("village"),
+]
 
-window.addEventListener("scroll", function(){
-    var value = window.scrollY;
-    stars.style.top = value * 1.2 + 'px';
-    moon.style.top = value * 1.1 + 'px';
-    mountains_behind.style.top = value * 0.45 +'px';
-    mountains_front.style.top = value * 0 + 'px';
+window.addEventListener("DOMContentLoaded", () => {
+    if (navigator.userAgent.indexOf("Firefox") !== -1){
+        window.alert("Firefox is not supported")
+    }
+    window.addEventListener("scroll", () => {
+        var value = window.scrollY;
+        var start_sensitivity = 2;
+        var drop_sensitivity = 0.25;
+        parallax.forEach((layer, index)=>{
+            layer.style.top = value * (start_sensitivity - (drop_sensitivity * index)) + "px"; //adjust
+        })
+    })
+    
+    window.addEventListener("mousemove", (e)=>{
+        const offsetX = (e.clientX - window.innerWidth / 2) / 15;
+        const offsetY = (e.clientY - window.innerHeight / 1.4) / 15;
+        parallax.forEach((layer, index)=>{
+            layer.style.transform = `translate(${-offsetX / (index * 1.1)}px, ${-offsetY /(index * 0.5)}px)`;
+        })
+    })
 })
