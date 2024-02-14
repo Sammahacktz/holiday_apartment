e2e41312de60306087c8d908f9e6dc560512ac74
chre029b <charlie-luis.reichelt@tu-dresden.de>
2024-02-14 19:09:57 +0100

    wip

--- script.js
+++ script.js
@@ -9,17 +9,32 @@ var parallax = [
     document.getElementById("village"),
 ]
 
+const cardTiming = 500
+
 window.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".texture-card")
+
+
     if (navigator.userAgent.indexOf("Firefox") !== -1){
         window.alert("Firefox is not supported")
     }
-    window.addEventListener("scroll", () => {
+    window.addEventListener("scroll", (e) => {
         var value = window.scrollY;
         var start_sensitivity = 2;
         var drop_sensitivity = 0.25;
         parallax.forEach((layer, index)=>{
             layer.style.top = value * (start_sensitivity - (drop_sensitivity * index)) + "px"; //adjust
         })
+        
+
+        cards.forEach((card)=>{
+            console.log(value + " test " + card.getBoundingClientRect().top)
+            if(value >= card.getBoundingClientRect().top)
+            setTimeout(()=>{
+                card.classList.add("card-slide")
+            },0)
+        })
+
     })
     
     window.addEventListener("mousemove", (e)=>{
